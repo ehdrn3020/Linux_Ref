ecfcf4e1359b90a36742eae1af40b3747876fff3
DongGu Kang <ehdrnsla3020@naver.com>
2017-06-30 13:31:00 +0900

    Add files via upload

---
+++ LinuxStable/Check_Linux_Specifications.sh
@@ -0,0 +1,17 @@
+﻿#OS Bit 
+$getconf LONG_BIT
+
+OS Series
+$cat /etc/issue
+
+OS Details
+$cat /proc/version
+
+CPU Info
+$cat /proc/cpuinfo
+
+RAM Memory
+$cat /proc/meminfo
+
+DISK Memory
+$df -h

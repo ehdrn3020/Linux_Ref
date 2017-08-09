a22e04580d8d326bcc981bc54ca0e5e5344185fd
DongGu Kang <ehdrnsla3020@naver.com>
2017-08-09 11:27:21 +0900

    Add files via upload

---
+++ Shell Script/IF_Statment.sh
@@ -0,0 +1,13 @@
+﻿#/bin/sh
+#if statment EX
+echo "Same number Move"
+echo -n "Insert number"
+read su
+if [ "$su" -eq 1 ];
+then echo "Move 1"
+elif [ "$su" -eq 2 ];
+then echo "Move 2"
+else
+ echo "Move 3"
+fi
+
